04fda11ee8bf87f7b588bbad20df770d215879c4
Hamza Quliyev <hquliyev2021@gmail.com>
2021-09-28 16:05:13 +0400

    Another commit

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ const nodemailer = require('nodemailer')
 
 const home = fs.readFileSync('./public/index.html')
 
+const tranporter = nodemailer.createTransport({
+    host: 'gmail',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'nodenodemailer123@gmail.com',
+        pass: 'nodemailer123'
+    }
+})
+
+async function sendMail(){
+    const info = tranporter.sendMail({
+        from: '"Nodemailer message"  <nodenodemailer123@gmail.com>',
+        to: 'hquliyev2021@gmail.com',
+        text: 'Hello world'
+    })
+    console.log('Message sent: %s', info.messageId)
+}
 
 app.get('/', (req,res) => {
     res.writeHead(200, 'content/type', 'text-html')
